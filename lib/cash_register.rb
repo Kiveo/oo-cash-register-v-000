3cba1ed19c5457085b2b81d06f6c744ebb0ccbac
Maverick Tesini <mavericktesini@gmail.com>
2018-03-20 15:47:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, quantity = nil)
+  def add_item(title, *quantity = nil)
     price = 1.0 * quantity
     @total += price
 
